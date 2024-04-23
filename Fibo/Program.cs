9fb3c0afc45eb18b14b3f2471c2c60f4f996b409
Felipe-Pestana <pestana1984@gmail.com>
2024-04-23 09:32:28 -0300

    Add project files.

---
+++ Fibo/Program.cs
@@ -0,0 +1,31 @@
+﻿int pos, penultimo = 0, ultimo = 1, atual;
+
+Console.WriteLine("Informe a posição limite: ");
+pos = int.Parse(Console.ReadLine());
+
+if (pos == 1)
+    Console.WriteLine(penultimo);
+else
+{
+    if (pos == 2)
+    {
+        Console.WriteLine(penultimo);
+        Console.WriteLine(ultimo);
+    }
+    else
+    {
+        Console.WriteLine(penultimo);
+        Console.WriteLine(ultimo);
+
+        for (int i = 2; i < pos; i++)
+        {
+            atual = penultimo + ultimo;
+            Console.WriteLine(atual);
+            penultimo = ultimo;
+            ultimo = atual;
+        }
+    }
+}
+
+
+
